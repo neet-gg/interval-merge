17c38c8e4a3e46c193e15d444372a506c25b1718
neet-gg <erze.azubi-0s@icloud.com>
2024-10-22 23:35:39 +0200

    Added implementation for MergeIntervals

--- merge.go
+++ merge.go
@@ -1,8 +1,24 @@
 package merge
 
+import "sort"
+
 type Interval struct {
 	Begin int
 	End   int
+}
+
+type ByBegin []Interval
+
+func (i ByBegin) Len() int {
+	return len(i)
+}
+
+func (i ByBegin) Swap(a, b int) {
+	i[a], i[b] = i[b], i[a]
+}
+
+func (i ByBegin) Less(a, b int) bool {
+	return i[a].Begin < i[b].Begin
 }
 
 // MergeInterval takes a list of intervals and merges
@@ -15,5 +31,35 @@ type Interval struct {
 //
 // Example: [{25,30}, {2,19}, {14,23}] -> [{2,23}, {25,30}]
 func MergeIntervals(intervals []Interval) []Interval {
-	return []Interval{}
+	// Base case
+	if len(intervals) <= 1 {
+		return intervals
+	}
+
+	// Sort intervals by beginning.
+	// This way we only need to compare each interval to its predecessor.
+	// Let I be the list of intervals:
+	// Since I[i].Begin < I[i+1].Begin, if I[i].End < I[i+1].Begin
+	// it follows that I[i].End < I[i+n].Begin for all n > 1.
+	sort.Sort(ByBegin(intervals))
+
+	result := []Interval{}
+	prev := intervals[0]
+
+	for i := 1; i < len(intervals); i++ {
+		// intervals[i] overlaps with previous interval
+		if intervals[i].Begin <= prev.End {
+			// intervals[i] is not a subset of previous interval
+			if intervals[i].End > prev.End {
+				prev.End = intervals[i].End
+			}
+		} else {
+			result = append(result, prev)
+			prev = intervals[i]
+		}
+	}
+
+	result = append(result, prev)
+
+	return result
 }
