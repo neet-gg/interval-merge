d35807e70724c0e307d9e7cf98a0f07ea14b2bb8
neet-gg <erze.azubi-0s@icloud.com>
2024-10-23 20:09:15 +0200

    Added two test cases for multiple disjoint intervals and intervals with negative values

--- merge_test.go
+++ merge_test.go
@@ -88,3 +88,37 @@ func TestMergeTangentIntervals(t *testing.T) {
 		}
 	}
 }
+
+// Test multiple intervals A, B, C, D
+// All intervals are disjoint and should not be merged
+func TestMergeMultipleDisjointIntervals(t *testing.T) {
+	input := []Interval{{45, 47}, {99, 120}, {10, 25}, {26, 31}}
+	merged := MergeIntervals(input)
+
+	if len(input) != len(merged) {
+		t.Fatalf("Expected merged intervals to be of size %d, got %d", len(input), len(merged))
+	}
+
+	for index, interval := range merged {
+		if interval != input[index] {
+			t.Errorf("Expected merged interval at index %d to be (%d, %d), got (%d, %d)", index, input[index].Begin, input[index].End, interval.Begin, interval.End)
+		}
+	}
+}
+
+// Test two intervals A and B, with B having a beginning < 0
+// A and B do overlap and should be merged
+func TestMergeNegativeValues(t *testing.T) {
+	merged := MergeIntervals([]Interval{{45, 120}, {-12, 45}})
+	expected := []Interval{{-12, 120}}
+
+	if len(expected) != len(merged) {
+		t.Fatalf("Expected merged intervals to be of size %d, got %d", len(expected), len(merged))
+	}
+
+	for index, interval := range merged {
+		if interval != expected[index] {
+			t.Errorf("Expected merged interval at index %d to be (%d, %d), got (%d, %d)", index, expected[index].Begin, expected[index].End, interval.Begin, interval.End)
+		}
+	}
+}
